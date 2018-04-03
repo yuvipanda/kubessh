2400616ae5fc5d68bf67183db6f0eb5666534044
yuvipanda <yuvipanda@gmail.com>
2018-04-03 01:30:50 -0700

    Implement ssh server ourselves
    The asyncssh project is wonderful. Implementing our own
    ssh server was easy. We can control authentication and spawning
    much better in our own implementation, unlikein OpenSSH.

---
+++ asyncserver.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import shlex
+import logging
+import asyncio
+import argparse
+
+import asyncssh
+
+from kubessh import Shell
+
+async def handle_client(process):
+    username = process.channel.get_extra_info('username')
+    shell = Shell(username, 'default', 'ubuntu:latest')
+    proc = shell.execute(shlex.split(process.get_command()))
+    await process.redirect(proc, proc, proc)
+    # Run this in an executor, since proc.wait blocks
+    loop = asyncio.get_event_loop()
+    ret = await loop.run_in_executor(None, proc.wait)
+    process.exit(ret)
+
+
+
+class Server(asyncssh.SSHServer):
+    def password_auth_supported(self):
+        return True
+
+    def validate_password(self, username, password):
+        return username == password
+
+async def start_server():
+    await asyncssh.create_server(
+        host='', 
+        port=8022,
+        server_factory=Server,
+        process_factory=handle_client,
+        kex_algs=[alg.decode('ascii') for alg in asyncssh.kex.get_kex_algs()],
+        server_host_keys='server',
+        session_encoding=None
+    )
+
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        '--debug',
+        action='store_true',
+        help="Enable debugging Logging"
+    )
+
+    args = argparser.parse_args()
+
+    logging.basicConfig(format='%(asctime)s %(message)s', level='DEBUG' if args.debug else 'INFO')
+
+    loop = asyncio.get_event_loop()
+
+    loop.run_until_complete(start_server())
+    loop.run_forever()
+
+if __name__ == '__main__':
+    main()
--- kubessh.py
+++ kubessh.py
@@ -1,86 +1,89 @@
 #!/usr/bin/env python3
+import asyncio
 import subprocess
+from ptyprocess import PtyProcess
 import time
 import argparse
 import os
 from kubernetes import client as k
 import kubernetes.config
 
-user = 'yuvipanda'
+try:
+    kubernetes.config.load_kube_config()
+except FileNotFoundError:
+    kubernetes.config.load_incluster_config()
 
-def make_pod(username):
-    return k.V1Pod(
-        metadata=k.V1ObjectMeta(
-            generate_name=user,
-            labels={
-                'kubessh.yuvi.in/username': username
-            }
-        ),
-        spec=k.V1PodSpec(
-            restart_policy='Never',
-            containers=[
-                k.V1Container(
-                    name='shell',
-                    image='ubuntu:latest',
-                    stdin=True,
-                    tty=True,
-                    command=['/bin/sh']
-                )
-            ],
-        )
-    )
-
-kubernetes.config.load_kube_config()
-
+# FIXME: Figure out if making this global is a problem
 v1 = k.CoreV1Api()
 
+class Shell:
+    """
+    A shell running in a pod
+    """
+    def __init__(self, name, namespace, image):
+        self.name = name
+        self.namespace = namespace
+        self.image = image
 
-def create_pod(namespace, pod_template):
-    created_pod = v1.create_namespaced_pod(namespace, pod_template)
-    return created_pod
+    def make_pod_spec(self):
+        return k.V1Pod(
+            metadata=k.V1ObjectMeta(
+                generate_name=self.name + '-',
+                labels={
+                    'kubessh.yuvi.in/username': self.name
+                }
+            ),
+            spec=k.V1PodSpec(
+                restart_policy='Never',
+                containers=[
+                    k.V1Container(
+                        name='shell',
+                        image=self.image,
+                        stdin=True,
+                        tty=True,
+                        command=['/bin/sh']
+                    )
+                ],
+            )
+        )
 
-def wait_for_running(pod):
-    while True:
-        if pod.status.phase == 'Running':
-            return
-        pod = v1.read_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
-        print(pod)
-        time.sleep(1)
+    def cleanup_pods(self, pods):
+        """
+        Delete all Failed and Succeeded pods
 
-def ensure_pod(username):
-    current_pods = v1.list_namespaced_pod('default', label_selector=f'kubessh.yuvi.in/username={username}')
-    if len(current_pods.items) == 0:
-        pod = create_pod('default', make_pod(username))
-    else:
-        for pod in current_pods.items:
+        Return list of pods that are not in Failed or Succeeded phases
+        """
+        remaining_pods = []
+        for pod in pods.items:
             if pod.status.phase == 'Failed' or pod.status.phase == 'Succeeded':
-                v1.delete_namespaced_pod(pod.metadata.name, 'default', k.V1DeleteOptions(grace_period_seconds=0))
+                v1.delete_namespaced_pod(pod.metadata.name, pod.metadata.namespace, k.V1DeleteOptions(grace_period_seconds=0))
             else:
-                return pod
-        pod = create_pod('default', pod_template)
+                remaining_pods.append(pod)
 
-def exec_in_pod(pod, command):
-    command = [
-        'kubectl',
-        'exec',
-        '--stdin',
-        '--tty',
-        pod.metadata.name,
-    ] + command
-    os.execvp(command[0], command)
+        return remaining_pods
 
-def main():
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument(
-        'command',
-        nargs=argparse.REMAINDER,
-        default=['/bin/bash']
-    )
+    def execute(self, command):
+        # Get list of current running pods that might be for our user
+        all_user_pods = v1.list_namespaced_pod(self.namespace, label_selector=f'kubessh.yuvi.in/username={self.name}')
 
-    args = argparser.parse_args()
+        current_user_pods = self.cleanup_pods(all_user_pods)
 
-    pod = ensure_pod('yuvipanda')
-    wait_for_running(pod)
-    exec_in_pod(pod, args.command)
+        if len(current_user_pods) == 0:
+            # No pods exist! Let's create some!
+            pod = v1.create_namespaced_pod(self.namespace, self.make_pod_spec())
+        else:
+            pod = current_user_pods[0]
+            # FIXME: What do we do if we have more than 1 running user pod?
 
-main()
+        while pod.status.phase != 'Running':
+            pod = v1.read_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
+            time.sleep(1)
+
+        command = [
+            'kubectl',
+            'exec',
+            '--stdin',
+            '--tty',
+            pod.metadata.name,
+        ] + command
+        return PtyProcess.spawn(argv=command)
