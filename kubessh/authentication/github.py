01863eb58359e2777f653657898cacc28d86adb9
yuvipanda <yuvipanda@gmail.com>
2018-04-13 18:39:01 -0700

    Properly validate SSH keys
    Earlier we were letting anyone in :)

--- kubessh/authentication/github.py
+++ kubessh/authentication/github.py
@@ -7,6 +7,9 @@ class GitHubAuthenticator(Authenticator):
     """
     Authenticate with GitHub SSH keys
     """
+    def connection_made(self, conn):
+        self.conn = conn
+
     def public_key_auth_supported(self):
         return True
 
@@ -15,14 +18,10 @@ class GitHubAuthenticator(Authenticator):
         Fetch and save user's keys for comparison later
         """
         url = f'https://github.com/{username}.keys'
-        async with aiohttp.ClientSession() as session, async_timeout.timeout(10):
+        async with aiohttp.ClientSession() as session, async_timeout.timeout(1):
             async with session.get(url) as response:
-                self.keys = asyncssh.import_authorized_keys(await response.text())
-
-    async def validate_public_key(self, key):
-        """
-        Validate the user can login with this key
-        """
-        # FIXME: Figure out what the client address validation is for
-        return self.keys.validate(key, '0.0.0.0')
-
+                keys = await response.text()
+                if keys:
+                    self.conn.set_authorized_keys(asyncssh.import_authorized_keys(keys))
+        # Return true to indicate we always *must* authenticate
+        return True
