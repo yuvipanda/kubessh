9eff19071d1da8cb340ce9370861d5162dde64ad
YuviPanda <yuvipanda@gmail.com>
2020-01-31 15:42:58 -0800

    Turn on debug mode by default
    So we can get real stack traces from exceptions

--- helm-chart/images/kubessh/kubessh_config.py
+++ helm-chart/images/kubessh/kubessh_config.py
@@ -3,6 +3,7 @@ from ruamel.yaml import YAML
 yaml = YAML()
 
 c.KubeSSH.host_key_path = '/etc/kubessh/secrets/kubessh.host-key'
+c.KubeSSH.debug = True
 
 with open('/etc/kubessh/config/values.yaml') as f:
     config = yaml.load(f)
